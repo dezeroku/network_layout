70c35fece4ff2a0b93c126ac4a40266e1a816a0e
dezeroku <dezeroku@gmail.com>
2022-12-02 01:29:38 +0100

    ci: Add SKIP_DOWNLOADS param to end-to-end-build.sh script

--- build/scripts/end-to-end-build.sh
+++ build/scripts/end-to-end-build.sh
@@ -3,6 +3,7 @@
 set -e
 
 [ -z "${SLEEP_BETWEEN_STAGES}" ] && SLEEP_BETWEEN_STAGES=3
+[ -z "${SKIP_DOWNLOADS:-}" ] && SKIP_DOWNLOADS="false"
 
 RUNDIR="$(readlink -f "$(dirname "$0")")"
 
@@ -12,21 +13,33 @@ echo "Build docker build environment image"
 sleep "${SLEEP_BETWEEN_STAGES}"
 "${RUNDIR}/setup-image.sh"
 
-echo "Clone the openwrt repo"
-sleep "${SLEEP_BETWEEN_STAGES}"
-"${RUNDIR}/clone.sh"
+if [[ ! "${SKIP_DOWNLOADS}" == "true" ]]; then
+    echo "Clone the openwrt repo"
+    sleep "${SLEEP_BETWEEN_STAGES}"
+    "${RUNDIR}/clone.sh"
+else
+    echo "Skipping cloning because of SKIP_DOWNLOADS=true"
+fi
 
-echo "Update source feeds"
-sleep "${SLEEP_BETWEEN_STAGES}"
-"${RUNDIR}/update-feeds.sh"
+if [[ ! "${SKIP_DOWNLOADS}" == "true" ]]; then
+    echo "Update source feeds"
+    sleep "${SLEEP_BETWEEN_STAGES}"
+    "${RUNDIR}/update-feeds.sh"
+else
+    echo "Skipping feeds update because of SKIP_DOWNLOADS=true"
+fi
 
 echo "Copy config for $1"
 sleep "${SLEEP_BETWEEN_STAGES}"
 "${RUNDIR}/copy-config.sh"
 
-echo "Download source code"
-sleep "${SLEEP_BETWEEN_STAGES}"
-"${RUNDIR}/build-download-sources.sh"
+if [[ ! "${SKIP_DOWNLOADS}" == "true" ]]; then
+    echo "Download source code"
+    sleep "${SLEEP_BETWEEN_STAGES}"
+    "${RUNDIR}/build-download-sources.sh"
+else
+    echo "Skipping source code download because of SKIP_DOWNLOADS=true"
+fi
 
 echo "Compile the final image"
 sleep "${SLEEP_BETWEEN_STAGES}"
