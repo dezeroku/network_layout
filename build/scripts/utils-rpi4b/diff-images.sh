c127b31d7a197a2fdcc6a3efb9685d3eb51b2a69
dezeroku <dezeroku@gmail.com>
2022-12-03 21:09:45 +0100

    scripts: device specific libs inheritance

--- build/scripts/utils-rpi4b/diff-images.sh
+++ build/scripts/utils-rpi4b/diff-images.sh
@@ -2,40 +2,10 @@
 set -euo pipefail
 
 # Unpack two images and run diff on them
-# This is reaaaly hacky at the moment
 
 SCRIPTS_DIR="$(readlink -f "$(dirname "$0")")/.."
 
 . "${SCRIPTS_DIR}"/common
+. "${SCRIPTS_DIR}/libs/rpi4b"
 
-function usage() {
-    echo "compare-images.sh image1.img.gz image2.img.gz"
-    exit 1
-}
-
-
-function cleanup() {
-    rm -rf "${TMPDIR}"
-}
-
-[ -z "${1:-}" ] && usage
-[ -z "${2:-}" ] && usage
-
-FIRST="$(readlink -f "${1}")"
-SECOND="$(readlink -f "${2}")"
-
-echoerr "${FIRST}"
-echoerr "${SECOND}"
-
-TMPDIR="$(mktemp -d)"
-
-pushd "${TMPDIR}" > /dev/null
-
-trap cleanup EXIT
-
-dirty_unpack_rpi4b_image "${FIRST}" first
-dirty_unpack_rpi4b_image "${SECOND}" second
-
-du -s first second
-
-diff -r first second --no-dereference
+. "${SCRIPTS_DIR}/libs/diff-images-runner"
--- build/scripts/utils-rpi4b/get-uci-config.sh
+++ build/scripts/utils-rpi4b/get-uci-config.sh
@@ -1,34 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Run 'uci show' on the sysroot
+# Unpack two images and run diff on them
+# This is reaaaly hacky at the moment
 
 SCRIPTS_DIR="$(readlink -f "$(dirname "$0")")/.."
 
 . "${SCRIPTS_DIR}"/common
+. "${SCRIPTS_DIR}/libs/rpi4b"
 
-function usage() {
-    echo "get-uci-config.sh image1.img.gz"
-    exit 1
-}
-
-function cleanup() {
-    rm -rf "${TMPDIR}"
-}
-
-[ -z "${1:-}" ] && usage
-
-IMAGE="$(readlink -f "${1}")"
-
-echoerr "Image: ${IMAGE}"
-
-TMPDIR="$(mktemp -d)"
-pushd "${TMPDIR}" > /dev/null
-
-trap cleanup EXIT
-
-dirty_unpack_rpi4b_image "${IMAGE}" .
-
-ensure_uci_runner
-
-docker run -it --rm -v "$PWD/etc:/etc" uci-runner show
+. "${SCRIPTS_DIR}/libs/get-uci-config-runner"
