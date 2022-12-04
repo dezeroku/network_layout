eddeb78488b75f146a9725df79c6d91742dfb0d3
dezeroku <dezeroku@gmail.com>
2022-12-04 16:03:25 +0100

    scripts: Add REPRODUCE_UPSTREAM_BUILD flag

--- build/scripts/core/copy-config.sh
+++ build/scripts/core/copy-config.sh
@@ -10,16 +10,24 @@ cd "${BUILDDIR}"
 
 # Copy the base upstream config, modifications and then expand
 cat "${SCRIPTS_DIR}/../${DEVICE}/config.orig" > .config
-if [ -f "${DEVICE_CONFIG_FILE}" ]; then
-    echoerr "Applying ${DEVICE_CONFIG_FILE}" on top of original .config
-    cat "${DEVICE_CONFIG_FILE}" >> .config
+if [[ ! "${REPRODUCE_UPSTREAM_BUILD}" == "true" ]]; then
+    if [ -f "${DEVICE_CONFIG_FILE}" ]; then
+        echoerr "Applying ${DEVICE_CONFIG_FILE}" on top of original .config
+        cat "${DEVICE_CONFIG_FILE}" >> .config
+    fi
+else
+    echoerr "Skipping custom .config because of REPRODUCE_UPSTREAM_BUILD=true"
 fi
 
 "${SCRIPTS_DIR}/core/run.sh" make defconfig
 
-# Remove the custom files present and copy from config dir
-if [ -d "${SCRIPTS_DIR}/../${DEVICE}/files" ]; then
-    echoerr "Copying custom files from ${SCRIPTS_DIR}/../${DEVICE}/files"
-    rm -rf "${BUILDDIR}/files"
-    cp -r "${SCRIPTS_DIR}/../${DEVICE}/files" "${BUILDDIR}/files"
+rm -rf "${BUILDDIR}/files"
+if [[ ! "${REPRODUCE_UPSTREAM_BUILD}" == "true" ]]; then
+    # Remove the custom files present and copy from config dir
+    if [ -d "${SCRIPTS_DIR}/../${DEVICE}/files" ]; then
+        echoerr "Copying custom files from ${SCRIPTS_DIR}/../${DEVICE}/files"
+        cp -r "${SCRIPTS_DIR}/../${DEVICE}/files" "${BUILDDIR}/files"
+    fi
+else
+    echoerr "Skipping copying custom files because of REPRODUCE_UPSTREAM_BUILD=true"
 fi
