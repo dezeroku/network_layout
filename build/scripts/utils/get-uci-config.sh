6188573ba95fe2c4a2991d595f141380bd4f925d
dezeroku <dezeroku@gmail.com>
2022-12-05 03:05:59 +0100

    scripts: don't inherit uci lib in common

--- build/scripts/utils/get-uci-config.sh
+++ build/scripts/utils/get-uci-config.sh
@@ -9,6 +9,7 @@ SCRIPTS_DIR="$(readlink -f "$(dirname "$0")")/.."
 . "${SCRIPTS_DIR}"/common
 parse_env_args
 
+. "${SCRIPTS_DIR}/libs/uci"
 . "${SCRIPTS_DIR}/libs/${DEVICE}"
 
 . "${SCRIPTS_DIR}/libs/get-uci-config-runner"
