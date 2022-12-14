a9ddb5d1d90bf77061713a5301a0471006202d00
dezeroku <dezeroku@gmail.com>
2022-12-14 03:43:00 +0100

    Move cache to builds/ dir

--- build/scripts/core/run.sh
+++ build/scripts/core/run.sh
@@ -5,10 +5,10 @@ SCRIPTS_DIR="$(readlink -f "$(dirname "$0")")/.."
 
 . "${SCRIPTS_DIR}/common"
 
-[ -z "${CCACHE_STORAGE:-}" ] && CCACHE_STORAGE="$(readlink -f "${SCRIPTS_DIR}/../ccache-target-storage")"
+[ -z "${CCACHE_STORAGE:-}" ] && CCACHE_STORAGE="$(readlink -f "${SCRIPTS_DIR}/../builds/ccache-target-storage")"
 [ -d "${CCACHE_STORAGE}" ] || mkdir "${CCACHE_STORAGE}"
 
-[ -z "${CCACHE_HOST_STORAGE:-}" ] && CCACHE_HOST_STORAGE="$(readlink -f "${SCRIPTS_DIR}/../sccache-host-storage")"
+[ -z "${CCACHE_HOST_STORAGE:-}" ] && CCACHE_HOST_STORAGE="$(readlink -f "${SCRIPTS_DIR}/../builds/sccache-host-storage")"
 [ -d "${CCACHE_HOST_STORAGE}" ] || mkdir "${CCACHE_HOST_STORAGE}"
 
 exec docker run --rm -it \
